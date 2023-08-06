c3631b3b6d34e204fd2a53fd7699ddc1af4be8a3
AlistairSymonds <alistair.symonds@gmail.com>
2023-08-06 12:03:28 -0400

    initalise device and SDL black screen

---
+++ src/vulkanohno.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <vulkan/vulkan.h>
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include <vk_types.h>
+#include <vk_initializers.h>
+#include "VkBootstrap.h"
+#include "vulkanohno.h"
+
+VulkanOhNo::VulkanOhNo()
+{
+}
+
+VulkanOhNo::~VulkanOhNo()
+{
+
+}
+
+int VulkanOhNo::init()
+{   
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+    //create blank SDL window for our application
+    _window = SDL_CreateWindow(
+        "Vulkan Adventures", //window title
+        SDL_WINDOWPOS_UNDEFINED, //window position x (don't care)
+        SDL_WINDOWPOS_UNDEFINED, //window position y (don't care)
+        _windowExtent.width,  //window width in pixels
+        _windowExtent.height, //window height in pixels
+        window_flags
+    );
+
+    init_vk();
+    //init_swapchain();
+    //everything went fine
+    _isInitialized = true;
+
+    return true;
+}
+
+int VulkanOhNo::run() {
+    {
+        SDL_Event e;
+        bool bQuit = false;
+
+        //main loop
+        while (!bQuit)
+        {
+            //Handle events on queue
+            while (SDL_PollEvent(&e) != 0)
+            {
+                //close the window when user clicks the X button or alt-f4s
+                if (e.type == SDL_QUIT) bQuit = true;
+            }
+
+            draw();
+        }
+    }
+    return 0;
+}
+
+int VulkanOhNo::draw() {
+    return 0;
+}
+
+void VulkanOhNo::cleanup() {
+    if (_isInitialized) {
+        SDL_DestroyWindow(_window);
+    }
+}
+
+int VulkanOhNo::init_vk()
+{
+
+    vkb::InstanceBuilder builder;
+    auto inst_ret = builder.set_app_name("Vulkan Adventures")
+        .request_validation_layers()
+        .use_default_debug_messenger()
+        .build();
+    if (!inst_ret) {
+        std::cerr << "Failed to create Vulkan instance. Error: " << inst_ret.error().message() << "\n";
+        return false;
+    }
+    vkb::Instance vkb_inst = inst_ret.value();
+    _instance = vkb_inst.instance;
+    
+
+    SDL_Vulkan_CreateSurface(_window, vkb_inst.instance, &_surface);
+
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    auto phys_ret = selector.set_surface(_surface)
+        .prefer_gpu_device_type(vkb::PreferredDeviceType::integrated)
+        .set_minimum_version(1, 3)
+        .require_dedicated_transfer_queue()
+        .select();
+    if (!phys_ret) {
+        std::cerr << "Failed to select Vulkan Physical Device. Error: " << phys_ret.error().message() << "\n";
+        return false;
+    }
+
+    vkb::DeviceBuilder device_builder{ phys_ret.value() };
+    // automatically propagate needed data from instance & physical device
+    auto dev_ret = device_builder.build();
+    if (!dev_ret) {
+        std::cerr << "Failed to create Vulkan device. Error: " << dev_ret.error().message() << "\n";
+        return false;
+    }
+    vkb::Device vkb_device = dev_ret.value();
+
+    // Get the VkDevice handle used in the rest of a vulkan application
+    device = vkb_device.device;
+
+    // Get the graphics queue with a helper function
+    auto graphics_queue_ret = vkb_device.get_queue(vkb::QueueType::graphics);
+    if (!graphics_queue_ret) {
+        std::cerr << "Failed to get graphics queue. Error: " << graphics_queue_ret.error().message() << "\n";
+        return false;
+    }
+    gfx_q = graphics_queue_ret.value();
+
+    VkPhysicalDeviceProperties props;
+    vkGetPhysicalDeviceProperties(phys_ret.value(), &props);
+    std::cout << "Vulkan initialised on: " << props.deviceName << std::endl;
+}
+
+int VulkanOhNo::init_swapchain()
+{
+    vkb::SwapchainBuilder swapchainBuilder{_chosenGPU, device, _surface };
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        .use_default_format_selection()
+        //use vsync present mode
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(_windowExtent.width, _windowExtent.height)
+        .build()
+        .value();
+
+    //store swapchain and its related images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+
+    _swapchainImageFormat = vkbSwapchain.image_format;
+    return 0;
+}
---
+++ src/vulkanohno.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <vulkan/Vulkan.h>
+#include <vector>
+
+class VulkanOhNo
+{
+public:
+	VulkanOhNo();
+	~VulkanOhNo();
+
+
+
+	int init();
+
+
+	int run();
+	int draw();
+	void cleanup();
+
+private:
+	bool _isInitialized{ false };
+	int _frameNumber{ 0 };
+
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	struct SDL_Window* _window{ nullptr };
+
+	int init_vk();
+	VkDevice device;
+	VkQueue gfx_q;
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+
+	VkSwapchainKHR _swapchain; // from other articles
+	VkFormat _swapchainImageFormat; // image format expected by the windowing system
+	std::vector<VkImage> _swapchainImages; //array of images from the swapchain
+	std::vector<VkImageView> _swapchainImageViews; 	//array of image-views from the swapchain
+
+
+	int init_swapchain();
+};
